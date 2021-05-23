f467efa05d54550ac07d69cb2345257c3739e2b8
amaralc <amaral.calil@gmail.com>
2021-05-23 18:22:26 -0300

    feature: add square array test

--- benchmark.js
+++ benchmark.js
@@ -63,5 +63,38 @@ function factorialTest(){
   runSuite(testLarge);
 }
 
+function squareArrayTest(){
+  function squareArrayJs(arr){
+    return arr.map(i => i*i);
+  }
+  const squareArrayAs = wasm.squareArray;
+
+  const testSmall = Benchmark.Suite('small squareArray');
+
+  testSmall
+    .add('AssemblyScript', function(){
+      squareArrayAs(Array(3).fill(2));
+    })
+    .add('JavaScript', function(){
+      squareArrayJs(Array(3).fill(2));
+    })
+
+  runSuite(testSmall);
+  
+  const testLarge = Benchmark.Suite('large squareArray');
+  const largeArray = new Int32Array(1000).fill(1).map((val, i) => val + (i * 0.001))
+
+  testLarge
+    .add('AssemblyScript', function(){
+      squareArrayAs(largeArray);
+    })
+    .add('JavaScript', function(){
+      squareArrayJs(largeArray);
+    })
+
+  runSuite(testLarge);
+}
+
 // addTest();
-factorialTest();
+// factorialTest();
+squareArrayTest();
