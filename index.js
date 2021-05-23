59328890126790aaf72b25f105766dc46999d9c2
amaralc <amaral.calil@gmail.com>
2021-05-23 18:09:05 -0300

    feature: add squareArray function and tests

--- index.js
+++ index.js
@@ -2,4 +2,18 @@ const fs = require("fs");
 const loader = require("@assemblyscript/loader");
 const imports = { /* imports go here */ };
 const wasmModule = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), imports);
-module.exports = wasmModule.exports;
+
+
+function squareArray(array){
+  const { __newArray, __getInt32Array, Int32Array_ID, squareArray: wasmSquareArray } = wasmModule.exports;
+
+  const typedArray = __newArray(Int32Array_ID, array)
+
+  const result = __getInt32Array(wasmSquareArray(typedArray));
+  return result;
+}
+
+module.exports = {
+  ...wasmModule.exports,
+  squareArray
+};
