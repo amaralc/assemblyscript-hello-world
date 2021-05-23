b12246533d9f0367124a56de8eaf264e4c7134cb
amaralc <amaral.calil@gmail.com>
2021-05-23 17:30:06 -0300

    feature: test compiled function

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,12 @@
 const assert = require("assert");
 const myModule = require("..");
+
+
 assert.strictEqual(myModule.add(1, 2), 3);
-console.log("ok");
+console.log('1 + 2 =', myModule.add(1,2));
+console.log("add: ok");
+console.log('---')
+
+assert.strictEqual(myModule.factorial(3), 6);
+console.log('factorial(3) =', myModule.factorial(3));
+console.log("factorial: ok")
